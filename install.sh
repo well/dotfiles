ad884804d676bcfa73d35f9f7031ce42ffffd1b6
Mat Trudel <mtrudel@matbook.local>
2010-06-18 17:14:10 -0400

    Added base git config

--- install.sh
+++ install.sh
@@ -5,4 +5,5 @@ ln -fs $DOTFILES/bashrc ${HOME}/.bashrc
 ln -fs $DOTFILES/bash_profile ${HOME}/.bash_profile
 ln -fs $DOTFILES/inputrc ${HOME}/.inputrc
 ln -fs $DOTFILES/gitignore ${HOME}/.gitignore
+ln -fs $DOTFILES/gitconfig ${HOME}/.gitconfig
 ln -fns $DOTFILES/bin ${HOME}/.bin
