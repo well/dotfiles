d1216462533127ea6878799322e40c6e1b1a263e
Mat Trudel <mat@geeky.net>
2010-06-22 16:30:08 -0400

    Better handling when no git around

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,14 +1,17 @@
-if [ ! -f /usr/bin/gcc ] ; then
-	echo "You need to install XCode before we can proceed. Please do so."
-	exit 1
-fi
-
-if [ -z `which brew` ] ; then
-	echo "Installing homebrew..."
-	ruby -e "$(curl -fsS http://gist.github.com/raw/436471/install_homebrew.rb)"
-fi
-
 if [ -z `which git` ] ; then
+	if [ ! -f /usr/bin/gcc ] ; then
+		echo "You need to install XCode before we can proceed."
+		echo "If you don't want to install XCode, then you can install"
+		echo "a prebuilt git binary from http://code.google.com/p/git-osx-installer/"
+		echo "and rerun this installer"
+		exit 1
+	fi
+
+	if [ -z `which brew` ] ; then
+		echo "Installing homebrew..."
+		ruby -e "$(curl -fsS http://gist.github.com/raw/436471/install_homebrew.rb)"
+	fi
+
 	echo "Installing git..."
 	sudo brew install git
 fi
